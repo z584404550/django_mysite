90963cac6f44fbbefe2fa25718d9bf8e8b656d8d
q5844 <q584404550@sina.com>
2021-08-09 10:35:18 +0800

    Default Changelist

---
+++ mysite/read_statistics/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.db.models.fields import exceptions
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+
+# Create your models here.
+
+
+class ReadNum(models.Model):
+    read_num = models.IntegerField(default=0)
+    content_type = models.ForeignKey(ContentType, on_delete=models.DO_NOTHING)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+    objects = models.Manager()
+
+
+class ReadNumExpandMethod():
+    def get_read_num(self):
+        try:
+            ct = ContentType.objects.get_for_model(self)
+            read_num = ReadNum.objects.get(content_type=ct, object_id=self.pk)
+            return read_num.read_num
+        except exceptions.ObjectDoesNotExist:
+            return 0
